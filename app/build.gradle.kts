dc3d3026d1dddd99234d885036491c7cbf4aa0a5
Javier Román Meléndez <jrm.romel@gmail.com>
2026-07-06 15:17:20 +0200

    Cambio nombre paquete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.ibizacustommap"
+        applicationId = "com.javirmmn.customobddashboard"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
